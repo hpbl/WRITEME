63b23647a91aadc9bfb4af0e281348b60ee56d2e
Hilton Pintor <hiltonpintor@gmail.com>
2019-05-22 18:02:54 -0300

    Testing sample fetch of repos from github

--- API/app.py
+++ API/app.py
@@ -1,14 +1,36 @@
-from flask import Flask
-# from redis import Redis
+from flask import Flask, jsonify
+from lib.Provider import fetch_repositories
+from functools import reduce
 
 app = Flask(__name__)
-# redis = Redis(host='redis', port=6379)
-
 
 @app.route('/')
 def hello():
-    # redis.incr('hits')
-    return 'Hello from the container'
+    return 'hello'
+
+def jsonResponses(responses, urls):
+    json = {}
+    json["repos"] = reduce(lambda accum, response: accum + response.json()["items"], responses, [])
+    json["urls"] = urls
+    return json
+
+
+@app.route('/<language>')
+def get_language_repos(language):
+    language_query = f'language:{language}'
+    sort = "stars"
+    number_repos = 100
+
+    (query_responses, urls) = fetch_repositories(language_query, sort, number_repos)
+
+    formatted_json = jsonResponses(query_responses, urls)
+
+    response = ''
+    for repo in formatted_json['repos']:
+        response += f'<li>{repo["html_url"]}</li>'
+    response += ''
+
+    return jsonify(response)
 
 
 if __name__ == "__main__":
