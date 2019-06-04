8df0ae8e3e606f355728b9fac07fa6236074eb44
Hilton Pintor <hiltonpintor@gmail.com>
2019-06-04 15:31:03 -0300

    Serveing grouped by level sections JSON

--- API/parser/Section.py
+++ API/parser/Section.py
@@ -1,3 +1,5 @@
+from flask.json import JSONEncoder
+
 class Section():
     def __init__(self, file_id, section_id, readme_file_name, heading_level, title, section_codes):
         self.file_id = file_id
@@ -9,3 +11,31 @@ class Section():
 
     def __eq__(self, other):
         return (self.file_id, self.section_id, self.readme_file_name, self.heading_level, self.title, self.section_codes) == (other.file_id, other.section_id, other.readme_file_name, other.heading_level, other.title, other.section_codes)
+
+    def to_json(self):
+        return {
+            'file_id': self.file_id,
+            'section_id': self.section_id,
+            'readme_file_name': self.readme_file_name,
+            'heading_level': self.heading_level,
+            'title': self.title,
+            'section_codes': self.section_codes,
+        }
+
+    @classmethod
+    def from_json(cls, json):
+        return Section(json['file_id'])
+
+
+class MyJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Section):
+            return {
+                'file_id': obj.file_id,
+                'section_id': obj.section_id,
+                'readme_file_name': obj.readme_file_name,
+                'heading_level': obj.heading_level,
+                'title': obj.title,
+                'section_codes': obj.section_codes,
+            }
+        return super(MyJSONEncoder, self).default(obj)
---
+++ API/sectionAnalyzer/test_SectionAnalyzer.py
@@ -0,0 +1,57 @@
+import unittest
+
+from API.sectionAnalyzer.SectionAnalyzer import group_sections_by_level
+from API.parser.Section import Section
+
+class SectionAnalyzerTestCase(unittest.TestCase):
+    """Section Analyzer Test Case"""
+    def setUp(self):
+        self.sections = [
+            Section(
+                file_id='1',
+                section_id='1',
+                readme_file_name='Quick.Quick.md',
+                heading_level=1,
+                title='Nimble',
+                section_codes=[1, 3]
+            ),
+            Section(
+                file_id='1',
+                section_id='2',
+                readme_file_name='Quick.Quick.md',
+                heading_level=2,
+                title='Swift Version',
+                section_codes=[3, 4]
+            ),
+            Section(
+                file_id='1',
+                section_id='1',
+                readme_file_name='Quick.Quick.md',
+                heading_level=2,
+                title='Nimble',
+                section_codes=[1, 3]
+            ),
+            Section(
+                file_id='1',
+                section_id='2',
+                readme_file_name='Quick.Quick.md',
+                heading_level=4,
+                title='Swift Version',
+                section_codes=[3, 4]
+            )
+        ]
+
+    def test_group_sections_by_level(self):
+        """
+            Given a List of sections,
+            When they are grouped by level
+            Then we should have a dict with arrays for each level
+        """
+        given = self.sections
+        when = group_sections_by_level(given)
+        self.assertEqual(len(when[1]), 1)
+        self.assertEqual(len(when[2]), 2)
+        self.assertEqual(len(when[3]), 0)
+        self.assertEqual(len(when[4]), 1)
+        self.assertEqual(len(when[5]), 0)
+        self.assertEqual(len(when[6]), 0)
