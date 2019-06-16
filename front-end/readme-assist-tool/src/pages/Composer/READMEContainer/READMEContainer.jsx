ca9b2a694c1612be20ae2535644f1c279459e887
Hilton Pintor <hiltonpintor@gmail.com>
2019-06-16 19:15:35 -0300

    Letting user download markdown

--- front-end/readme-assist-tool/src/pages/Composer/READMEContainer/READMEContainer.jsx
+++ front-end/readme-assist-tool/src/pages/Composer/READMEContainer/READMEContainer.jsx
@@ -3,29 +3,40 @@ import PropTypes from 'prop-types';
 import './READMEContainer.css';
 
 function markdownHeaderToHTML(title, level) {
+  const markdownTitle = `${'#'.repeat(level)} ${title}`;
   const key = `${title}-${level}`;
   switch (level) {
     case 1:
-      return <h1 key={key}>{title}</h1>;
+      return <h1 key={key}>{markdownTitle}</h1>;
     case 2:
-      return <h2 key={key}>{title}</h2>;
+      return <h2 key={key}>{markdownTitle}</h2>;
     case 3:
-      return <h3 key={key}>{title}</h3>;
+      return <h3 key={key}>{markdownTitle}</h3>;
     case 4:
-      return <h4 key={key}>{title}</h4>;
+      return <h4 key={key}>{markdownTitle}</h4>;
     case 5:
-      return <h5 key={key}>{title}</h5>;
+      return <h5 key={key}>{markdownTitle}</h5>;
     case 6:
-      return <h6 key={key}>{title}</h6>;
+      return <h6 key={key}>{markdownTitle}</h6>;
     default:
       console.log(`Unexpected headingLevel ${level}`);
       return undefined;
   }
 }
 
+function downloadMarkdownFile() {
+  const element = document.createElement('a');
+  const file = new Blob([document.getElementById('READMEcontent').innerText], { type: 'text/plain' });
+  element.href = URL.createObjectURL(file);
+  element.download = 'README.md';
+  document.body.appendChild(element); // Required for this to work in FireFox
+  element.click();
+}
+
 const READMEContainer = ({ selectedSections }) => (
   <div className="READMEContainer">
-    <div className="content">
+    <button type="button" onClick={downloadMarkdownFile}>Download</button>
+    <div id="READMEcontent">
       <div className="TopSection">
         <h1># Project Name</h1>
         <p>&gt; tagline</p>
