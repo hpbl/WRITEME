1e7f217a20cee85a60309da26f062eaecb5afe05
Hilton Pintor <hiltonpintor@gmail.com>
2019-06-24 00:28:41 -0300

    Using HashRouter

--- front-end/readme-assist-tool/src/index.jsx
+++ front-end/readme-assist-tool/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import Composer from './pages/Composer/Composer';
 import About from './pages/About/About';
@@ -10,15 +10,12 @@ console.log('p', process.env.PUBLIC_URL);
 
 function App() {
   return (
-    <BrowserRouter basename={`${process.env.PUBLIC_URL}/`} onUpdate={() => window.scrollTo(0, 0)}>
+    <HashRouter basename={`${process.env.PUBLIC_URL}/`} onUpdate={() => window.scrollTo(0, 0)}>
       <Switch>
         <Route path="/" exact component={About} />
-        <Route path={`${process.env.PUBLIC_UR}/pub`} exact component={About} />
-        <Route path="README-Assist-Tool/teste" exact component={About} />
-        <Route path="/teste2" exact component={About} />
         <Route path="/composer/:language" component={Composer} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
