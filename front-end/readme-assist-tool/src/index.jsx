0e7a127fa21eceb952689eb0fd8839aebcb42f15
Hilton Pintor <hiltonpintor@gmail.com>
2019-06-23 23:40:43 -0300

    Reverting basename

--- front-end/readme-assist-tool/src/index.jsx
+++ front-end/readme-assist-tool/src/index.jsx
@@ -9,7 +9,7 @@ import * as serviceWorker from './serviceWorker';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_UR} onUpdate={() => window.scrollTo(0, 0)}>
+    <BrowserRouter basename="/README-Assist-Tool" onUpdate={() => window.scrollTo(0, 0)}>
       <Switch>
         <Route path="/" exact component={About} />
         <Route path={`${process.env.PUBLIC_UR}/pub`} exact component={About} />
