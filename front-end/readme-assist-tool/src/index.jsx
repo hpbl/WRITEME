a09e9440d1616d63a55c1075c0d33a04c5e6f4e5
Hilton Pintor <hiltonpintor@gmail.com>
2019-06-24 00:19:20 -0300

    Testing new routes

--- front-end/readme-assist-tool/src/index.jsx
+++ front-end/readme-assist-tool/src/index.jsx
@@ -6,14 +6,16 @@ import Composer from './pages/Composer/Composer';
 import About from './pages/About/About';
 import * as serviceWorker from './serviceWorker';
 
+console.log('p', process.env.PUBLIC_URL);
 
 function App() {
   return (
-    <BrowserRouter basename="/README-Assist-Tool" onUpdate={() => window.scrollTo(0, 0)}>
+    <BrowserRouter basename={`${process.env.PUBLIC_URL}/`} onUpdate={() => window.scrollTo(0, 0)}>
       <Switch>
         <Route path="/" exact component={About} />
         <Route path={`${process.env.PUBLIC_UR}/pub`} exact component={About} />
         <Route path="README-Assist-Tool/teste" exact component={About} />
+        <Route path="/teste2" exact component={About} />
         <Route path="/composer/:language" component={Composer} />
       </Switch>
     </BrowserRouter>
