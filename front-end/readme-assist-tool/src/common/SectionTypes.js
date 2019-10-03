bc8f52b94118d32ab94950b6d94a16abd07d09c5
bethurban <beth.urban@gmail.com>
2019-10-03 09:13:28 -0400

    Style tooltips

--- front-end/readme-assist-tool/src/common/SectionTypes.js
+++ front-end/readme-assist-tool/src/common/SectionTypes.js
@@ -1,7 +1,7 @@
 export const sectionTypes = {
-  1: ['WHAT/WHY', 'An introduction on what your project does; the motivation behind your project, it&apos;s advantages.'],
+  1: ['WHAT/WHY', 'An introduction on what your project does; the motivation behind your project, it\'s advantages.'],
   3: ['HOW', 'Instructions on how to use the project.'],
-  4: ['WHEN', 'The status of the project, it&apos;s versions and roadmap.'],
+  4: ['WHEN', 'The status of the project, it\'s versions and roadmap.'],
   5: ['WHO', 'The people responsible for the project, license information, code of conduct.'],
   6: ['REFERENCES', 'External documentation, support, and related projects.'],
   7: ['CONTRIBUTION', 'Instructions on how to contribute to the project (sometimes a stand-alone file).'],
